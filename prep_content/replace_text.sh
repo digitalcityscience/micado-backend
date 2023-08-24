c258f899a01be586b2ae425d969a1165fd96427d
balcse <cserpesbalazs@gmail.com>
2023-08-24 13:22:34 +0200

    cleaned up keycloak realm config - added placeholder - renamed template - put keycloak realm json into named volume

--- prep_content/replace_text.sh
+++ prep_content/replace_text.sh
@@ -2,8 +2,8 @@
 
 cp /tmp/source/keycloak_realms.json /tmp/realms/
 
-sed -i 's+pa.micado-pilot.hamburg.de+'"$PA_HOSTNAME"'+g' /tmp/realms/keycloak_realms.json
-sed -i 's+ngo.micado-pilot.hamburg.de+'"$NGO_HOSTNAME"'+g' /tmp/realms/keycloak_realms.json
-sed -i 's+migrants.micado-pilot.hamburg.de+'"$MIGRANTS_HOSTNAME"'+g' /tmp/realms/keycloak_realms.json
+sed -i 's+PLACEHOLDER_PA+'"$PA_HOSTNAME"'+g' /tmp/realms/keycloak_realms.json
+sed -i 's+PLACEHOLDER_NGO+'"$NGO_HOSTNAME"'+g' /tmp/realms/keycloak_realms.json
+sed -i 's+PLACEHOLDER_MIGRANTS+'"$MIGRANTS_HOSTNAME"'+g' /tmp/realms/keycloak_realms.json
 
 echo "done"
